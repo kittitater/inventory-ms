66aca741e56b8d163965a923cf9b59286a54bd66
kittitater <kittitater@gmail.com>
2023-04-21 15:56:05 +0700

    1

--- src/components/SideNavbar.js
+++ src/components/SideNavbar.js
@@ -328,15 +328,15 @@ export default function SideNavbar() {
         >
           <img
             alt="Man"
-            src="https://images.unsplash.com/photo-1600486913747-55e5470d6f40?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+            src="https://kittitater.github.io/mybio/bio.jpg"
             class="h-10 w-10 rounded-full object-cover"
           />
 
           <div>
             <p class="text-xs">
-              <strong class="block font-medium">Eric Frusciante</strong>
+              <strong class="block font-medium">Kittitat Songsakseree</strong>
 
-              <span> eric@frusciante.com </span>
+              <span> kittitat.song@kmutt.ac.th </span>
             </p>
           </div>
         </Link>
