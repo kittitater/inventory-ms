9c6fddde2475ab95c64fdf2b37161c114f8639ce
kittitater <kittitater@gmail.com>
2023-04-21 15:33:14 +0700

    1

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,13 @@
 
 // import { Inter } from 'next/font/google'
+import Welcome from '@/components/Welcome';
 import Head from 'next/head'
 // import Welcome from '@/components/Welcome'
 
 export default function Home() {
   return (
   <>
-    
+    <Welcome/>
     
 
     
